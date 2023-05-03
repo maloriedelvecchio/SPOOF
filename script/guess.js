b46ae989a5f1b15c0198366142e9eb3323851551
maloriedelvecchio <118386401+maloriedelvecchio@users.noreply.github.com>
2023-05-03 13:47:12 -0400

    fixed instructions; added more possible answers

--- script/guess.js
+++ script/guess.js
@@ -26,7 +26,7 @@ function writeGameOn(value) {
 
 /*--------------- Read Data - Guesser (Dave & Malorie w/Help) -------------*/
 
-async function readSpinData(){
+async function readSpinData() {
   const dbRef = ref(getDatabase());
   const response = await get(child(dbRef, `spinData/1`)).then((snapshot) => {
     if (snapshot.exists()) {
@@ -41,7 +41,7 @@ async function readSpinData(){
   return response;
 }
 
-async function readTimeStamp(){
+async function readTimeStamp() {
   const dbRef = ref(getDatabase());
   const storedTime = await get(child(dbRef, `timeStamp/`)).then((snapshot) => {
     if (snapshot.exists()) {
@@ -58,51 +58,51 @@ async function readTimeStamp(){
 
 /*--------------- Possible Answers (Dave & Malorie) -------------*/
 
-let possibleAnswers = [ 
-  ["You are brushing your teeth",   "You're brushing your teeth",   "Brushing teeth",  "Brushing your teeth",  "Cleaning your teeth",   "Toothbrush"],   
-  ["You are doing a magic trick",   "You're doing a magic trick",   "Doing a magic trick",  "Doing magic",  "Performing a magic trick",   "Magic",   "Casting a spell"],   
-  ["You are playing the piano",   "You're playing the piano",   "Playing piano",   "Piano",   "Keyboard"],   
-  ["You are playing basketball",   "You're playing basketball",   "Playing basketball",   "Basketball"],   
-  ["You are taking a selfie",   "You're taking a selfie",   "Taking selfie",   "Selfie",   "Photo"],   
-  ["You are doing a yoga pose",   "You're doing a yoga pose",   "Doing a yoga pose",   "Yoga pose",   "Yoga"],   
-  ["You are baking a cake",   "You're baking a cake",   "Baking a cake",   "Making a cake",   "Baking"],   
-  ["You are walking a dog",   "You're walking a dog",   "Walking dog",   "Dog walking"],   
-  ["You are opening a present",   "You're opening a present",   "Opening a present", "Opening a gift"],
-  ["You are playing video games", "You're playing video games", "Playing video games", "Playing Xbox", "Playing Play Station"],
-  ["You are fishing", "You're fishing", "Fishing", "Fishing rod"],
-  ["You are rock climbing", "You're rock climbing", "Rock climbing", "Climbing"]
+let possibleAnswers = [
+  ["You are brushing your teeth", "You're brushing your teeth", "Brushing teeth", "Brushing your teeth", "Cleaning your teeth", "Toothbrush", "Using toothpaste", "Using toothbrush"],
+  ["You are doing a magic trick", "You're doing a magic trick", "Doing a magic trick", "Doing magic", "Doing magic trick", "Performing a magic trick", "Performing magic", "Doing a trick", "Magic", "Casting a spell"],
+  ["You are playing the piano", "You're playing the piano", "Playing piano", "Piano", "Playing keyboard", "Keyboard", "Playing the piano", "Playing the keyboard"],
+  ["You are playing basketball", "You're playing basketball", "Playing basketball", "Basketball", "Shooting hoops", "Dribbling", "Dribbling the ball"],
+  ["You are taking a selfie", "You're taking a selfie", "Taking a selfie", "Taking selfie", "Selfie", "Photo", "Picture", "Taking picture", "Taking photo", "Taking a picture", "Taking a photo"],
+  ["You are doing a yoga pose", "You're doing a yoga pose", "Doing a yoga pose", "Doing yoga", "Doing yoga pose", "Yoga pose", "Yoga"],
+  ["You are baking a cake", "You're baking a cake", "Baking a cake", "Making a cake", "Baking"],
+  ["You are walking a dog", "You're walking a dog", "Walking dog", "Walking a dog", "Dog walking", "Walking on a leash"],
+  ["You are opening a present", "You're opening a present", "Opening a present", "Opening present", "Opening a gift", "Opening gift", "Unwrapping", "Unwrapping a gift", "Unwrapping a present", "Unwrapping gift", "Unwrapping present"],
+  ["You are playing video games", "You're playing video games", "Playing video games", "Playing video game", "Playing videogames", "Playing videogame", "Video games", "Video game", "Videogames", "Videogame"],
+  ["You are fishing", "You're fishing", "Fishing", "Fishing rod", "Catching fish", "Catching a fish", "Casting a line", "Reeling in a fish"],
+  ["You are rock climbing", "You're rock climbing", "Rock climbing", "Rockclimbing", "Climbing", "Mountain climbing", "Climbing a mountain", "Climbing a boulder", "Climbing boulder", "Scaling a cliff"]
 ];
 
 /*-------------------- Win/Lose (Malorie w/Help) -----------------*/
 
-  document.getElementById("guessForm").addEventListener("submit", function(e){
-    e.preventDefault();
-    let x = document.forms["guessForm"]["text-input"].value;   //  let x be the text inputted
-    readSpinData().then(function (response){                   //  access db and get value
-      console.log(x);                                          //  writes x (user input?) to log
-      console.log(response);                                   //  writes PROMPT as value ----- // object (i) and value = "Correct prompt" if correct and {value: 'Correct prompt'} if not correct
+document.getElementById("guessForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let x = document.forms["guessForm"]["text-input"].value;   //  let x be the text inputted
+  readSpinData().then(function (response) {                   //  access db and get value
+    console.log(x);                                          //  writes x (user input?) to log
+    console.log(response);                                   //  writes PROMPT as value ----- // object (i) and value = "Correct prompt" if correct and {value: 'Correct prompt'} if not correct
 
-      if(possibleAnswers[response.index].includes(x)){      
-        window.location.href = "guesser_winner.html";
-        writeGameOn(false);
-        return true;
-      }
-        
-      else {
-        const wrongGuess = document.querySelector('#wrong');
-        let li = document.createElement('li');
-        li.className = "list-group-item";
-        li.innerHTML = '<p class="score">' + x + '</p>';
-        wrongGuess.appendChild(li);
-        wrongGuess.classList.remove("hide");
-        document.getElementById("guessForm").reset();
-      };
-    });
+    if (possibleAnswers[response.index].includes(x)) {
+      window.location.href = "guesser_winner.html";
+      writeGameOn(false);
+      return true;
+    }
+
+    else {
+      const wrongGuess = document.querySelector('#wrong');
+      let li = document.createElement('li');
+      li.className = "list-group-item";
+      li.innerHTML = '<p class="score">' + x + '</p>';
+      wrongGuess.appendChild(li);
+      wrongGuess.classList.remove("hide");
+      document.getElementById("guessForm").reset();
+    };
   });
+});
 
-  /*-------------- Read Data - Game On (Dave & Malorie w/Help) --------------*/
+/*-------------- Read Data - Game On (Dave & Malorie w/Help) --------------*/
 
-async function readGameOn(){
+async function readGameOn() {
   const dbRef = ref(getDatabase());
   const response = await get(child(dbRef, `gameOn/`)).then((snapshot) => {
     if (snapshot.exists()) {
@@ -129,7 +129,7 @@ function checkTimerState() {
         const elapsed = Math.floor(difference / 1000);
         let time = 180 - elapsed;
         console.log(time);
-        
+
         function updateTimer() {
           let minutes = Math.floor(time / 60);
           let seconds = time % 60;
@@ -155,10 +155,10 @@ function checkTimerState() {
 
 /*---------------- Response to Cancel by Actor ------------------*/
 
-function checkGameState(){
-  readGameOn().then(function (response){                                                     
+function checkGameState() {
+  readGameOn().then(function (response) {
     console.log(response);
-    if(response.value){       // game IS on
+    if (response.value) {       // game IS on
 
     }
     else {                    // game IS NOT on
@@ -169,7 +169,7 @@ function checkGameState(){
 
 /*----------------- General (Malorie w/Help) ------------------*/
 
-$(document).ready(function(){
+$(document).ready(function () {
   $('[data-toggle="popover"]').popover();
   setInterval(checkGameState, 1000); /// this one calls the function every second
   checkTimerState();
