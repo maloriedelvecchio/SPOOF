d4b5d0ca41310c8ba8749271b97ffdda1307e0aa
maloriedelvecchio <118386401+maloriedelvecchio@users.noreply.github.com>
2023-05-04 14:05:51 -0400

    added possible answers

--- script/guess.js
+++ script/guess.js
@@ -60,15 +60,15 @@ async function readTimeStamp() {
 
 let possibleAnswers = [
   ["You are brushing your teeth", "You're brushing your teeth", "Brushing teeth", "Brushing your teeth", "Cleaning your teeth", "Toothbrush", "Using toothpaste", "Using toothbrush"],
-  ["You are doing a magic trick", "You're doing a magic trick", "Doing a magic trick", "Doing magic", "Doing magic trick", "Performing a magic trick", "Performing magic", "Doing a trick", "Magic", "Casting a spell"],
-  ["You are playing the piano", "You're playing the piano", "Playing piano", "Piano", "Playing keyboard", "Keyboard", "Playing the piano", "Playing the keyboard"],
+  ["You are doing a magic trick", "You're doing a magic trick", "Doing a magic trick", "Doing magic", "Doing magic trick", "Performing a magic trick", "Performing magic", "Doing a trick", "Magic", "Casting a spell", "Spell", "Magician", "Illusion"],
+  ["You are playing the piano", "You're playing the piano", "Playing piano", "Piano", "Playing keyboard", "Keyboard", "Playing the piano", "Playing the keyboard", "Pianist"],
   ["You are playing basketball", "You're playing basketball", "Playing basketball", "Basketball", "Shooting hoops", "Dribbling", "Dribbling the ball"],
   ["You are taking a selfie", "You're taking a selfie", "Taking a selfie", "Taking selfie", "Selfie", "Photo", "Picture", "Taking picture", "Taking photo", "Taking a picture", "Taking a photo"],
   ["You are doing a yoga pose", "You're doing a yoga pose", "Doing a yoga pose", "Doing yoga", "Doing yoga pose", "Yoga pose", "Yoga"],
-  ["You are baking a cake", "You're baking a cake", "Baking a cake", "Making a cake", "Baking"],
-  ["You are walking a dog", "You're walking a dog", "Walking dog", "Walking a dog", "Dog walking", "Walking on a leash"],
+  ["You are baking a cake", "You're baking a cake", "Baking a cake", "Making a cake", "Baking", "Baker", "Bakery"],
+  ["You are walking a dog", "You're walking a dog", "Walking dog", "Walking a dog", "Dog walking", "Walking on a leash", "Walking the dog", "Dog walker"],
   ["You are opening a present", "You're opening a present", "Opening a present", "Opening present", "Opening a gift", "Opening gift", "Unwrapping", "Unwrapping a gift", "Unwrapping a present", "Unwrapping gift", "Unwrapping present"],
-  ["You are playing video games", "You're playing video games", "Playing video games", "Playing video game", "Playing videogames", "Playing videogame", "Video games", "Video game", "Videogames", "Videogame"],
+  ["You are playing video games", "You're playing video games", "Playing video games", "Playing video game", "Playing videogames", "Playing videogame", "Video games", "Video game", "Videogames", "Videogame", "Gaming"],
   ["You are fishing", "You're fishing", "Fishing", "Fishing rod", "Catching fish", "Catching a fish", "Casting a line", "Reeling in a fish"],
   ["You are rock climbing", "You're rock climbing", "Rock climbing", "Rockclimbing", "Climbing", "Mountain climbing", "Climbing a mountain", "Climbing a boulder", "Climbing boulder", "Scaling a cliff"]
 ];
